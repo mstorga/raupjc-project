0236eb72cc13db9f44ff6a16226e4331029b86fc
Matej Štorga <matej.storga@fer.hr>
2018-02-17 14:42:45 +0100

    reupload

--- Library/Library/Startup.cs
+++ Library/Library/Startup.cs
@@ -33,16 +33,10 @@ namespace Library
             services.AddScoped<ICheckout, CheckoutService>();
             services.AddScoped<IPatron, PatronService>();
             services.AddScoped<ILibraryBranch, LibraryBranchService>();
-            // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
                 services.AddDbContext<LibraryContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            else
-                services.AddDbContext<LibraryContext>(options
-                => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
 
-            // Automatically perform database migration
-            services.BuildServiceProvider().GetService<LibraryContext>().Database.Migrate();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
